836eb469cbdf48fa4e354d0a58389e31a5df3c41
akhil-silicon <akhil.vault@gmail.com>
2025-11-09 17:53:04 +0530

    Create 1_busSignal_a.v

---
+++ 1_busSignal_a.v
@@ -0,0 +1,21 @@
+`timescale 1ns / 1ps
+module 1_busSignal_a(
+ in_1,
+ in_2,
+ in_3,
+ out_1
+
+    );
+    input [3:0] in_1;
+    input [3:0] in_2;
+    input in_3;
+    output [3:0] out_1;
+    
+    wire [3:0] in_3_bus;
+    
+    //
+    
+    assign in_3_bus= {4{in_3}};
+    assign out_1= (in_1 & ~in_3_bus) | (in_2 & in_3_bus);
+    
+endmodule
